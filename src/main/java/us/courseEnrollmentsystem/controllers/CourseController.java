2bb8b09c315651bfbd4d8eb8bd6648de01568255
A_Abdulazeez <adebex999@gmail.com>
2026-09-06 21:51:37 +0100

    expose the end points for CourseController

--- src/main/java/us/courseEnrollmentsystem/controllers/CourseController.java
+++ src/main/java/us/courseEnrollmentsystem/controllers/CourseController.java
@@ -1,12 +1,15 @@
 package us.courseEnrollmentsystem.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import us.courseEnrollmentsystem.data.models.Course;
 import us.courseEnrollmentsystem.dtos.requests.CreateCourseRequest;
+import us.courseEnrollmentsystem.dtos.requests.UpdateCourseRequest;
 import us.courseEnrollmentsystem.dtos.responses.CreateCourseResponse;
+import us.courseEnrollmentsystem.dtos.responses.UpdateCourseResponse;
 import us.courseEnrollmentsystem.services.CourseService;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
@@ -17,5 +20,25 @@ public class CourseController {
     @PostMapping("/create-course")
     public CreateCourseResponse createCourse (@RequestBody CreateCourseRequest createCourseRequest){
         return courseService.createCourse(createCourseRequest);
+    }
+
+    @GetMapping("/course-code/{courseCode}")
+    public Course getCourse(@PathVariable("courseCode") String courseCode) {
+        return courseService.getCourseByCode(courseCode);
+    }
+
+    @GetMapping("/get-courses")
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PutMapping("/update-course/{courseCode}")
+    public UpdateCourseResponse updateCourse(@PathVariable("courseCode") String courseCode, @RequestBody UpdateCourseRequest updateCourseRequest) {
+        return courseService.updateCourse(courseCode, updateCourseRequest);
+    }
+
+    @DeleteMapping("/delete-course/{courseCode}")
+    public String deleteCourse(@PathVariable("courseCode") String courseCode) {
+        return courseService.deleteCourse(courseCode);
     }
 }
