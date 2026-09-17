c305dd8f54c55b4c64d4ffe91e088f26a19db7e4
A_Abdulazeez <adebex999@gmail.com>
2026-09-17 13:37:01 +0100

    created StudentController

--- src/main/java/us/courseEnrollmentsystem/controllers/StudentController.java
+++ src/main/java/us/courseEnrollmentsystem/controllers/StudentController.java
@@ -1,9 +1,37 @@
 package us.courseEnrollmentsystem.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import us.courseEnrollmentsystem.data.models.Student;
+import us.courseEnrollmentsystem.dtos.requests.UpdateStudentRequest;
+import us.courseEnrollmentsystem.dtos.responses.UpdateStudentResponse;
+import us.courseEnrollmentsystem.services.StudentService;
+
+import java.util.List;
 
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @GetMapping("/get-student/{email}")
+    public Student getStudentByEmail(@PathVariable("email") String email) {
+        return studentService.getStudentByEmail(email);
+    }
+
+
+    @PutMapping("/update-student/{email}")
+    public UpdateStudentResponse updateStudent(@PathVariable("email") String email, @RequestBody UpdateStudentRequest updateRequest) {
+        return studentService.updateStudent(email, updateRequest);
+    }
+
+
+    @GetMapping("/get-all-students")
+    public List<Student> getAllStudents(@RequestHeader("email") String email) {
+        return studentService.getAllStudents(email);
+    }
 
 }
